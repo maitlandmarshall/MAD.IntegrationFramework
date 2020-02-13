ffb667b7adeca85d26d0c2dec013f2e27c5f36c7
maitlandmarshallapps <maitlandmarshallapps@gmail.com>
2020-02-13 19:14:14 +1000

    SalesforceApiClient now uses the refresh token to get the next AccessToken automatically

--- src/MaitlandsInterfaceFramework.Salesforce/SalesforceApiFactory.cs
+++ src/MaitlandsInterfaceFramework.Salesforce/SalesforceApiFactory.cs
@@ -2,10 +2,8 @@
 using Salesforce.Common;
 using Salesforce.Force;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MaitlandsInterfaceFramework.Salesforce
@@ -19,15 +17,18 @@ namespace MaitlandsInterfaceFramework.Salesforce
             if (config is null)
                 throw new NotImplementedException($"{nameof(MIFConfig)} must implement {nameof(ISalesforceConfig)}.");
 
-            using AuthenticationClient auth = new AuthenticationClient();
-            auth.InstanceUrl = "https://ap16.salesforce.com";
+            using AuthenticationClient auth = new AuthenticationClient
+            {
+                InstanceUrl = "https://ap16.salesforce.com"
+            };
+            await auth.TokenRefreshAsync(config.SalesforceConsumerKey, config.SalesforceRefreshToken, config.SalesforceConsumerSecret);
 
-            ForceClient client = new ForceClient(auth.InstanceUrl, config.SalesforceAccessToken, auth.ApiVersion);
-            var jsonHttpClientField = client.GetType().GetField("_jsonHttpClient", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            ForceClient client = new ForceClient(auth.InstanceUrl, auth.AccessToken, auth.ApiVersion);
+            FieldInfo jsonHttpClientField = client.GetType().GetField("_jsonHttpClient", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
 
             JsonHttpClient jsonHttpClient = jsonHttpClientField.GetValue(client) as JsonHttpClient;
 
-            var httpClientField = jsonHttpClient.GetType().GetField("HttpClient", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            FieldInfo httpClientField = jsonHttpClient.GetType().GetField("HttpClient", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
 
             HttpClient httpClient = httpClientField.GetValue(jsonHttpClient) as HttpClient;
 
