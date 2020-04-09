9d1294cd05579d36a5cafb14376272dbaf3e7cf8
Maitland Marshall <maitlandmarshallapps@gmail.com>
2020-04-09 12:15:42 +1000

    Adding to test console app project

---
+++ src/MAD.IntegrationFramework.TestApp/ExampleIntegration.cs
@@ -0,0 +1,22 @@
+﻿using MAD.IntegrationFramework.Integrations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAD.IntegrationFramework.TestApp
+{
+    public class ExampleIntegration : TimedIntegration
+    {
+        public override TimeSpan Interval => TimeSpan.FromMinutes(1);
+        public override bool IsEnabled => true;
+
+        public override Task Execute()
+        {
+            // Execute your unit of work here. 
+            // Any errors will be automatically logged to the database if a SQL Connection String is provided in the configuration class.
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MAD.IntegrationFramework.TestApp/ExampleMIFConfig.cs
@@ -0,0 +1,17 @@
+﻿using MAD.IntegrationFramework.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAD.IntegrationFramework.TestApp
+{
+    public class ExampleMIFConfig : MIFConfig
+    {
+        public string EmailAddress { get; set; }
+
+        public ExampleMIFConfig()
+        {
+            this.SqlConnectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+        }
+    }
+}
